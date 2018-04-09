4021b6980abce56a56990dce79deeecff0858528
Unknown <enzo.billis@viacesi.fr>
2018-04-09 12:44:49 +0200

    Add 50 row for seeding bdd

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,19 +11,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
-        $this->call(ActivitiesTableSeeder::class);
-        $this->call(BuyTableSeeder::class);
-        $this->call(CatalogTableSeeder::class);
-        $this->call(CategoryTableSeeder::class);
-        $this->call(CensorshipTableSeeder::class);
-        $this->call(CommentTableSeeder::class);
-        $this->call(IdeasTableSeeder::class);
-        $this->call(InscriptionsTableSeeder::class);
-        $this->call(LikeTableSeeder::class);
-        $this->call(NotificationTableSeeder::class);
-        $this->call(PicturesTableSeeder::class);
-        $this->call(PublishActivitiesTableSeeder::class);
-        $this->call(VotesTableSeeder::class);
+        for ($i=0;$i<50;$i++){
+            $this->call(UsersTableSeeder::class);
+            $this->call(ActivitiesTableSeeder::class);
+            $this->call(BuyTableSeeder::class);
+            $this->call(CatalogTableSeeder::class);
+            $this->call(CategoryTableSeeder::class);
+            $this->call(CensorshipTableSeeder::class);
+            $this->call(CommentTableSeeder::class);
+            $this->call(IdeasTableSeeder::class);
+            $this->call(InscriptionsTableSeeder::class);
+            $this->call(LikeTableSeeder::class);
+            $this->call(NotificationTableSeeder::class);
+            $this->call(PicturesTableSeeder::class);
+            $this->call(PublishActivitiesTableSeeder::class);
+            $this->call(VotesTableSeeder::class);
+        }
+
     }
 }
