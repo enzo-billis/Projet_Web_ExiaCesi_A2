14dcce1433294840358657edbb8cf047b057dace
Unknown <enzo.billis@viacesi.fr>
2018-04-10 23:07:37 +0200

    Add feature to filter dynamicly with JS
    Request to API

--- resources/views/manifestations.blade.php
+++ resources/views/manifestations.blade.php
@@ -2,12 +2,11 @@
 
 @section('content')
 
-
     <div class="container">
-        <div class="row justify-content-md-center">
+        <div class="row justify-content-ml-center">
             <div class="form-group">
                 <label for="sel1">Filtrer : </label>
-                <select onchange="request();" class="form-control" id="sell" name="manifStatus">
+                <select onchange="expandResults(this.value)"  class="form-control" id="sell" name="manifStatus">
                     <option value="0">Tout</option>
                     <option value="1">A venir</option>
                     <option value="2">Passés</option>
@@ -16,40 +15,104 @@
             </div>
         </div>
         <div id="container" class="row">
-            {{--@foreach($manifs as $manif)--}}
-            {{--<div class="col-md-5 offset-1" id="1">--}}
-                {{--<div class="card">--}}
-                    {{--<a href={{route('manif',$manif->id)}} ><div class="card-header" style="text-align: center">--}}
-                        {{--<img src={{ $manif->image}} ><br>--}}
-                        {{--{{ $manif->name }} |  {{ $manif->date_add }}<br>--}}
-                        {{--</div> </a>--}}
 
-                    {{--<div class="card-body">--}}
-                        {{--Nom : {{ $manif->name }}<br>--}}
-                        {{--Description : {{ $manif->description }} <br>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-                <script>
-                    function request(){
-                        var container = document.getElementById("container");
-                        var list = document.getElementById("sell");
-                        var request = new XMLHttpRequest();
-                        request.open("GET","{{route('APIManifs')}}", false);
-                        request.send(null);
-
-                        var responses = JSON.parse(request.responseText);
-                        responses.forEach(function(response){
-                            //console.log(list.value);
-                            if (parseInt(response.status) == list.value){
-                                container.innerHTML='<p>some dynamic html</p>';
-                            }
-                        });
-
-                    }
-                </script>
-            {{--@endforeach--}}
         </div>
 
     </div>
+
+    <script>
+
+        var request = new XMLHttpRequest();
+        request.open("GET", "{{route('APIManifs')}}", false);
+        request.send(null);
+        var responses = JSON.parse(request.responseText);
+
+        expandResults("0");
+
+        function expandResults(filterValue){
+            containerE = document.getElementById("container");
+
+            containerE.innerHTML = "";
+            var date = new Date();
+            actualDate = date.toISOString().substring(0, 10);
+            fDate = new Date(date.getFullYear(), date.getMonth(), 2);
+            firstDate = fDate.toISOString().substring(0, 10);
+            lDate = new Date(date.getFullYear(), date.getMonth() + 1, 1);
+            lastDate = lDate.toISOString().substring(0, 10);
+            for (var i in responses){
+                switch (filterValue) {
+                    case "0" :
+                            containerE.innerHTML = containerE.innerHTML +
+                                "<div class='col-md-5 offset-1' id='1'>" +
+                                "<div class='card'>" +
+                                "<a href=manif/" + responses[i].id + ""+" ><div class='card-header' style='text-align: center'>"+
+                                "<img src="+responses[i].image+"><br>"+
+                                responses[i].name+" | "+responses[i].date_add+"<br>"+
+                                "</div> </a>"+
+                                "<div class='card-body'>"+
+                                "Nom : "+responses[i].name+"<br>"+
+                                "Description : "+responses[i].description+"<br>"+
+                                "</div>"+
+                                "</div>"+
+                                "</div>";
+                        break;
+                    case "1" :
+                        if (responses[i].date_add >= actualDate) {
+
+                            containerE.innerHTML = containerE.innerHTML +
+                                "<div class='col-md-5 offset-1' id='1'>" +
+                                "<div class='card'>" +
+                                "<a href=manif/" + responses[i].id + ""+" ><div class='card-header' style='text-align: center'>"+
+                                "<img src="+responses[i].image+"><br>"+
+                                responses[i].name+" | "+responses[i].date_add+"<br>"+
+                                "</div> </a>"+
+                                "<div class='card-body'>"+
+                                "Nom : "+responses[i].name+"<br>"+
+                                "Description : "+responses[i].description+"<br>"+
+                                "</div>"+
+                                "</div>"+
+                                "</div>";
+                        }
+                        break;
+                    case "2" :
+                        if (responses[i].date_add < actualDate) {
+
+                            containerE.innerHTML = containerE.innerHTML +
+                                "<div class='col-md-5 offset-1' id='1'>" +
+                                "<div class='card'>" +
+                                "<a href=manif/" + responses[i].id + ""+" ><div class='card-header' style='text-align: center'>"+
+                                "<img src="+responses[i].image+"><br>"+
+                                responses[i].name+" | "+responses[i].date_add+"<br>"+
+                                "</div> </a>"+
+                                "<div class='card-body'>"+
+                                "Nom : "+responses[i].name+"<br>"+
+                                "Description : "+responses[i].description+"<br>"+
+                                "</div>"+
+                                "</div>"+
+                                "</div>";
+                        }
+                        break;
+                    case "3" :
+                        if (responses[i].date_add >= firstDate && responses[i].date_add <= lastDate) {
+
+                            containerE.innerHTML = containerE.innerHTML +
+                                "<div class='col-md-5 offset-1' id='1'>" +
+                                "<div class='card'>" +
+                                "<a href=manif/" + responses[i].id + ""+" ><div class='card-header' style='text-align: center'>"+
+                                "<img src="+responses[i].image+"><br>"+
+                                responses[i].name+" | "+responses[i].date_add+"<br>"+
+                                "</div> </a>"+
+                                "<div class='card-body'>"+
+                                "Nom : "+responses[i].name+"<br>"+
+                                "Description : "+responses[i].description+"<br>"+
+                                "</div>"+
+                                "</div>"+
+                                "</div>";
+
+                        }
+                        break;
+                }
+            }
+        }
+    </script>
 @endsection
