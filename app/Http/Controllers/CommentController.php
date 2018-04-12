ac748c30b400a54b4f4a1f3a82fa97290c66bc5b
Unknown <enzo.billis@viacesi.fr>
2018-04-12 20:39:22 +0200

    Change order by for printing comments

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -5,16 +5,18 @@ namespace App\Http\Controllers;
 use Auth;
 use Illuminate\Http\Request;
 use App\Comment;
+use Illuminate\Support\Facades\Validator;
 
 class CommentController extends Controller
 {
     public function getComments($id_picture){
         $commentsObj = New Comment;
-        $comments = $commentsObj->where('id_pictures','=', $id_picture)->orderBy('date_comment','desc')->get();
+        $comments = $commentsObj->where('id_pictures','=', $id_picture)->orderBy('created_at','desc')->get();
         return $comments;
     }
 
     public static function addComment($content,$id_pic){
+
         $commentsObj = New Comment;
         $mytime = date('Y-m-d');
         $commentsObj::create([
