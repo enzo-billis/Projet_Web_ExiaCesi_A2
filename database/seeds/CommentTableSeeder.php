9916707e34be363c2068f9ec77aa38ec69e50f8a
Unknown <enzo.billis@viacesi.fr>
2018-04-12 11:14:11 +0200

    Change seeder

--- database/seeds/CommentTableSeeder.php
+++ database/seeds/CommentTableSeeder.php
@@ -12,12 +12,13 @@ class CommentTableSeeder extends Seeder
     public function run()
     {
         $faker = \Faker\Factory::create();
-
-        DB::table('comment')->insert([
-            'id_pictures' => $faker->numberBetween(1,50),
-            'id_users' => $faker->numberBetween(1,50),
-            'comment' => $faker->text,
-            'date_comment' => $faker->date('Y-m-d'),
-        ]);
+        for ($i = 0; $i < 200; $i++) {
+            DB::table('comment')->insert([
+                'id_pictures' => $faker->numberBetween(1, 50),
+                'id_users' => $faker->numberBetween(1, 50),
+                'comment' => $faker->text,
+                'date_comment' => $faker->date('Y-m-d'),
+            ]);
+        }
     }
 }
