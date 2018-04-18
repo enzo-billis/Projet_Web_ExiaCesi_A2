51444d3050545717471572767c59a06ab3b627dc
Unknown <enzo.billis@viacesi.fr>
2018-04-18 13:00:41 +0200

    Edit front-end add product

--- resources/views/newProduct.blade.php
+++ resources/views/newProduct.blade.php
@@ -1,24 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="col-md- offset-1" id="1">
-        <div class="card">
-            <div class="card-header">nouveau produits</div>
-            <div class="card-body">
-                <form action="{{route('PostNewProduct')}}" method="post" enctype="multipart/form-data">
-                    @csrf
-                    <label for="name" id="lname" class="form-control">Name <input id="name" type="text" name="name"></label><br>
-                    <label for="price" id="lprice" class="form-control">price<input id="price" type="number" name="price" min="0.01" max="999.99" step="0.01"></label><br>
-                    <label for="description" id="ldescription" class="form-control">description<textarea id="description" name="description"></textarea></label><br>
-                    <label for="category" id="lcategory" class="form-control">category<select name='category' id='category'>
-                        <option value='1'>1-Cloths</option>
-                        <option value='2'>2-Goodies</option>
-                        <option value='3'>3-Miscellaneous</option>
-                        </select></label><br>
-                    <input type="file" id="image" name="image" class="form-control">
-                    <input id="submit" type="submit">
-                </form>
+    <div class="container">
+            <div class="card">
+                <div class="card-header">Ajouter un produit</div>
+                <div class="card-body">
+                    <form action="{{route('PostNewProduct')}}" method="post" enctype="multipart/form-data">
+                        {{csrf_field()}}
+                        <label for="name" id="lname">Nom :<input  class="form-control" id="name" type="text" name="name"></label><br>
+                        <label for="price" id="lprice" >Prix :<input  class="form-control" id="price" type="number" name="price" min="0.01" max="999.99" step="0.01"></label><br>
+                        <label for="description" id="ldescription" >Description :<textarea  class="form-control" id="description" name="description"></textarea></label><br>
+                        <label for="category" id="lcategory" >Catégorie :<select  class="form-control" name='category' id='category'>
+                                <option value='1'>Vêtements</option>
+                                <option value='2'>Goodies</option>
+                                <option value='3'>Divers</option>
+                            </select></label><br>
+                        <label>Image du produit :</label>
+                        <input type="file" id="image" name="image" class="form-control-file">
+                        <br>
+                        <button type="submit" class="btn btn-success">Ajouter</button>
+                    </form>
+                </div>
             </div>
-        </div>
     </div>
+
 @endsection
