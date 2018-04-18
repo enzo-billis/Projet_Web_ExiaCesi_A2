5679c5967a9674c22e015a6ccb2defd7fd397ee4
Unknown <enzo.billis@viacesi.fr>
2018-04-18 17:40:08 +0200

    Add total price product in cart

--- resources/views/buy.blade.php
+++ resources/views/buy.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-
+<?php $total=0 ?>
     <div class="container">
         <div class="col-md" id="1">
             <div class="card">
@@ -15,6 +15,7 @@
                                 <th>#</th>
                                 <th>Produit</th>
                                 <th>Quantité</th>
+                                <th>Prix unitaire</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -23,10 +24,13 @@
                                 <td>{{$loop->iteration}}</td>
                                 <td>{{$command->catalog->name}}</td>
                                 <td>{{$command->quantity}}</td>
+                                <td>{{$command->catalog->price}}€</td>
                             </tr>
+                            <?php $total=($command->catalog->price)*($command->quantity)+$total ?>
                         @endforeach
                         </tbody>
                     </table>
+                    Total : {{$total}} €
                 </div>
             </div>
         </div>
