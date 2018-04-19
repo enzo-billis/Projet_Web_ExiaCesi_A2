46efcffb5e58e62a5521b0532f64c3834a3d5e0a
Unknown <enzo.billis@viacesi.fr>
2018-04-19 16:45:27 +0200

    Auto stash before rebase of "tempo"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,20 +2,46 @@
 
 @section('content')
     <div class="container">
-        <div class="content">
-            <div class="title m-b-md">
-                BDE Cesi Rouen
-            </div>
-            @if (Route::has('login'))
-                <div class="links">
-                    @auth
-                        <a href="{{ url('/home') }}">Acceder au site</a>
-                    @else
-                        <a href="{{ route('login') }}">Connexion</a>
-                        <a href="{{ route('register') }}">Inscription</a>
-                    @endauth
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="https://picsum.photos/1080/400" alt="First slide" width="100%" style="max-width: 1080px">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="https://picsum.photos/1080/401" alt="Second slide" width="100%" style="max-width: 1080px">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="https://picsum.photos/1080/399" alt="Third slide" width="100%" style="max-width: 1080px">
+                    </div>
                 </div>
-            @endif
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        <div class="container">
+
+            <h1 class="my-4">Bienvenue sur le site du BDE Cesi Rouen !</h1>
+            <div class="row">
+                <div class="jumbotron">
+                    <h1 class="display-4">Qui sommes-nous ?</h1>
+                    <p class="lead">Site officiel du bureau des étudiants du CESI Rouen. </p>
+                    <hr class="my-4">
+                    <p>Ici, vous pourrez retrouver les prochaines activités que nous proposons, vous inscrire, poster des photos et des idées. Enfin une magnifique boutique est en place pour que vous puissiez vous procurez nos produits à un prix exorbitant !</p>
+                    <p class="lead">
+                        <a class="btn btn-primary btn-lg" href="/manif" role="button">Visiter</a>
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
